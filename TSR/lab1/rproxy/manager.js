af9f994d887a664ccf46669c5555ae1ca8f23f89
Carlos Galindo <carlos.s.galindo@gmail.com>
2016-10-15 14:00:14 +0200

    TSR: Lab1 Session3 improved rproxy to be persistent, added rm to manager

--- TSR/lab1/rproxy/manager.js
+++ TSR/lab1/rproxy/manager.js
@@ -1,15 +1,24 @@
 var net = require('net');
 
 var PROXY_PORT = 8000;
+var msg;
 
-var msg = JSON.stringify({
-    'op':"set",
-    "inPort":parseInt(process.argv[3]),
-    'remote':{
-        "ip":process.argv[4],
-        "port":process.argv[5]
-    }
-});
 
-var socket = net.connect({port:PROXY_PORT, address:process.argv[2]},
+if (process.argv[2] == 'set' && process.argv.length == 6) {
+    msg = JSON.stringify({
+        'op':process.argv[2],
+        "inPort":parseInt(process.argv[3]),
+        'remote':{
+            "ip":process.argv[4],
+            "port":process.argv[5]
+        }
+    });
+} else if (process.argv[2] == 'rm' && process.argv.length == 4) {
+    msg = JSON.stringify({
+        'op':process.argv[2],
+        'inPort':parseInt(process.argv[3])
+    });
+}
+
+var socket = net.connect({port:PROXY_PORT, address:'localhost'},
     function() {socket.write(msg); socket.end();});
--- TSR/lab1/rproxy/rproxy.js
+++ TSR/lab1/rproxy/rproxy.js
@@ -1,8 +1,24 @@
 var net = require('net');
+var fs  = require('fs');
 
 var map = [];
+var servers = [];
 var CONFIG_PORT = 8000;
 var LOCAL_IP = '127.0.0.1';
+
+fs.readFile('rproxy.json', 'utf8', (err, data) => {
+    if (err) console.log("[ERROR] " + err);
+    else {
+        var newMap = JSON.parse(data);
+        newMap.forEach((element, index, array) => {
+            var server = route(element.inPort, element.ip, element.port)
+            map.push({'inPort':element.inPort,'ip':element.ip,'port':element.port});
+            servers.push(server);
+            logConnection(map.length - 1);
+        });
+    }
+});
+
 
 var configServer = net.createServer(function(socket) {
     socket.on('data', function(msg) {
@@ -11,19 +27,35 @@ var configServer = net.createServer(function(socket) {
             console.log("[WARNING] The config port (" + CONFIG_PORT + ")cannot be rerouted.");
             return;
         }
-        var i;
-        for (i = 0; i < map.length; i++) {
-            if (map[i].inPort == jmsg.inPort) {
-                map[i].server.close();
-                console.log("[WARNING] " + map[i].inPort + " not " + map[i].ip + ":" + map[i].port);
-                map.splice(i, 1);
-                break;
+        if (jmsg.op == 'set') {
+            var i;
+            for (i = 0; i < map.length; i++) {
+                if (map[i].inPort == jmsg.inPort) {
+                    servers[i].close();
+                    console.log("[WARNING] " + map[i].inPort + " not " + map[i].ip + ":" + map[i].port);
+                    map.splice(i, 1);
+                    servers.splice(i, 1);
+                    break;
+                }
+            }
+            var server = route(jmsg.inPort, jmsg.remote.ip, jmsg.remote.port);
+            map.push({'inPort':jmsg.inPort,'ip':jmsg.remote.ip,'port':jmsg.remote.port});
+            servers.push(server);
+            logConnection(map.length - 1);
+            saveConfig();
+        } else if (jmsg.op == 'rm') {
+            var i;
+            for (i = 0; i < map.length; i++) {
+                if (map[i].inPort == jmsg.inPort) {
+                    servers[i].close();
+                    console.log("[INFO] " + map[i].inPort + " not " + map[i].ip + ":" + map[i].port);
+                    map.splice(i, 1);
+                    servers.splice(i, 1);
+                    saveConfig();
+                    break;
+                }
             }
         }
-        var server = route(jmsg.inPort, jmsg.remote.ip, jmsg.remote.port);
-        map.push({'inPort':jmsg.inPort,'ip':jmsg.remote.ip,'port':jmsg.remote.port,'server':server});
-        i = map.length - 1;
-        console.log("[INFO] " + map[i].inPort + " --> " + map[i].ip + ":" + map[i].port);
     });
 }).listen(CONFIG_PORT, LOCAL_IP);
 
@@ -36,6 +68,14 @@ function route(inPort, ip, port) {
         });
     }).listen(inPort, LOCAL_IP);
     return server;
+}
+
+function saveConfig() {
+    fs.writeFile('rproxy.json', JSON.stringify(map), (err) => {if (err) console.log("[ERROR] " + err);});
+}
+
+function logConnection(i) {
+    console.log("[INFO] " + map[i].inPort + " --> " + map[i].ip + ":" + map[i].port);
 }
 
 console.log("TCP server accepting configurations on port: " + CONFIG_PORT);
