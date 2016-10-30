5819f7a51aa80ad3ca7f51815771a0ea29ad2546
Carlos Galindo <carlos.s.galindo@gmail.com>
2016-10-30 17:43:44 +0100

    BDA Lab begun BIBLIOTECA queries

---
+++ BDA/biblioteca.sql
@@ -0,0 +1,144 @@
+-- QUERIES USING 1 RELATION
+
+-- Exercise 1
+SELECT nombre
+FROM autor
+WHERE nacionalidad = 'Argentina';
+
+-- Exercise 2
+SELECT titulo
+FROM obra
+WHERE titulo LIKE '%mundo%';
+
+-- Exercise 3
+SELECT id_lib, num_obras
+FROM libro
+WHERE "AÑO" < 1990
+AND num_obras > 1;
+
+-- Exercise 4
+SELECT COUNT(*) LIB_AÑO
+FROM libro
+WHERE "AÑO" IS NOT NULL;
+
+-- Exercise 5
+SELECT COUNT(*) MÁS_1_OB
+FROM libro
+WHERE num_obras > 1;
+
+-- Exercise 6
+SELECT id_lib
+FROM libro
+WHERE "AÑO" = 1997
+AND titulo IS NULL;
+
+-- Exercise 7
+SELECT titulo
+FROM libro
+WHERE titulo IS NOT NULL
+ORDER BY titulo DESC;
+
+-- Exercise 8
+SELECT SUM(num_obras) OBRAS
+FROM libro
+WHERE "AÑO" BETWEEN 1990 AND 1999;
+
+
+-- QUERIES USING MORE THAN 1 RELATION
+
+-- Exercise 9
+SELECT COUNT(DISTINCT autor_id)
+FROM escribir
+WHERE cod_ob IN (
+    SELECT cod_ob
+    FROM obra
+    WHERE titulo LIKE '%ciudad%'
+);
+
+-- Exercise 10
+SELECT titulo
+FROM obra
+WHERE cod_ob IN (
+    SELECT cod_ob
+    FROM escribir
+    WHERE autor_id IN (
+        SELECT autor_id
+        FROM autor
+        WHERE nombre = 'Camús, Albert'
+    )
+);
+
+-- Exercise 11
+SELECT nombre
+FROM autor
+WHERE autor_id IN (
+    SELECT autor_id
+    FROM escribir
+    WHERE cod_ob IN (
+        SELECT cod_ob
+        FROM obra
+        WHERE titulo = 'La tata'
+    )
+);
+
+-- Exercise 12
+SELECT nombre
+FROM amigo
+WHERE num IN (
+    SELECT num
+    FROM leer
+    WHERE cod_ob IN (
+        SELECT cod_ob
+        FROM escribir
+        WHERE autor_id = 'RUKI'
+    )
+);
+
+-- Exercise 13
+SELECT id_lib, titulo
+FROM libro
+WHERE titulo IS NOT NULL
+AND 1 < (
+    SELECT COUNT(cod_ob)
+    FROM esta_en
+    WHERE libro.id_lib = id_lib
+);
+
+
+-- QUERIES WITH SUBQUERIES
+
+-- Exercise 14
+SELECT titulo, nombre
+FROM autor, obra
+WHERE nacionalidad = 'Francesa'
+AND cod_ob IN (
+    SELECT cod_ob
+    FROM escribir
+    WHERE autor_id = autor.autor_id
+    AND cod_ob NOT IN (
+        SELECT cod_ob
+        FROM escribir
+        WHERE autor_id <> autor.autor_id
+    )
+);
+
+-- Exercise 15
+SELECT COUNT(autor_id) SIN_OBRA
+FROM autor
+WHERE autor_id NOT IN (
+    SELECT autor_id
+    FROM escribir
+);
+
+-- Exercise 16
+SELECT nombre
+FROM autor
+WHERE nacionalidad = 'Española'
+AND 2 <= (
+    SELECT COUNT(cod_ob)
+    FROM escribir
+    WHERE autor_id = autor.autor_id
+);
+
+-- Exercise 17
+SELECT 
