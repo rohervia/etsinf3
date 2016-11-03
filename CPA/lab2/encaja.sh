ae5f0afa048ce7cf6e675519f4a35f0673434fd8
Carlos Galindo <carlos.s.galindo@gmail.com>
2016-11-03 12:00:23 +0100

    CPA Lab2 corrected Makefile and encaja.sh

--- CPA/lab2/encaja.sh
+++ CPA/lab2/encaja.sh
@@ -2,42 +2,39 @@
 #PBS -l nodes=1,walltime=00:10:00
 #PBS -q cpa
 #PBS -d .
-#PBS -o csv/res.csv
+#PBS -o res.csv
 #PBS -e error
 
-if [[ $1 == seq ]] then;
-    make -s e1 e3
-    echo 'program,time'
+make -s exercises
 
-    for file in $(ls src/encaja-e?); do
-        printf "%s," $file;
-        $file -o out/Lenna.ppm;
-        if ! cmp out/Lenna.ppm img/binLenna1024.ppm; then
-            echo "%s execution was not correct" $file;
-        fi
-    done
+echo 'seq-program,time'
 
-    make -s clean
-elif [[ $1 == par ]]; then
-    make -s e2 e4
-    echo 'program,2,4,8,16,32'
+for file in $(ls out/encaja-e?); do
+	printf "%s," $file;
+	$file -o out/Lenna.ppm;
+	printf "\n";
+	if ! cmp out/Lenna.ppm img/binLenna1024.ppm; then
+		echo "%s execution was not correct" $file;
+	fi
+done
 
-    for file in $(ls src/encaja-e?-p?); do
-        printf "%s," $file;
-        for threads in $(echo 2 4 8 16 32); do
-            OMP_NUM_THREADS=$threads $file -o out/Lenna.ppm;
-            if ! cmp out/Lenna.ppm img/binLenna1024.ppm; then
-                printf "%s execution with %d threads was not correct" $file $threads;
-            fi
-            if [ $threads -ne 32 ]; then
-                printf ",";
-            fi
-        done
-        printf "\n";
-    done
+printf "\n";
 
-    make -s clean
-else
-    echo -e 'Usage: qsub encaja.sh seq\|par';
-fi
+echo 'par-program,2,4,8,16,32'
+
+for file in $(ls out/encaja-e?-p?); do
+	printf "%s," $file;
+	for threads in $(echo 2 4 8 16 32); do
+		OMP_NUM_THREADS=$threads $file -o out/Lenna.ppm;
+		if ! cmp out/Lenna.ppm img/binLenna1024.ppm; then
+			printf "%s execution with %d threads was not correct" $file $threads;
+		fi
+		if [ $threads -ne 32 ]; then
+			printf ",";
+		fi
+	done
+	printf "\n";
+done
+
+make -s clean
 
